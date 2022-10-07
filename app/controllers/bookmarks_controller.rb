854881bf3a3c3a099948bd3e28d903698848bcb6
Joana Areas <jo_areas@hotmail.com>
2022-10-07 20:40:12 -0300

    routes e controller bookmark2

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,28 @@
 class BookmarksController < ApplicationController
+
   def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.movie = Movie.last
+    if @bookmark.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/edit'
-
   root "pages#home"
 
   resources :movies, only: %i[index show]
   resources :lists do
-    resources :bookmarks, only: %i[new edit]
+    resources :bookmarks, only: %i[new create edit]
   end
 end
